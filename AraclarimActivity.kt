0182c18fb869bb83444a411c1e73a0f7d8358203
Burak Aydin <90382482+GrgnPl@users.noreply.github.com>
2022-12-23 11:31:24 +0300

    Update AraclarimActivity.kt
    Retrofit Kütüpanesi kullananlar kendine bir sınıf oluşturup yapması lazım. Ben kdi diye bir değişken oluşturup onu çağırdım. eğer response olursa işlem adapter sınıfını çağırıp context ve listemizi yazıyoruz

--- AraclarimActivity.kt
+++ AraclarimActivity.kt
@@ -1,4 +1,4 @@
-package com.kacyakiyor.aracimkacyakti
+
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +9,6 @@ import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.navigation.NavigationView
-import com.izleyerekkazan.izlekazanprojesi.API.ApiUtils
-import com.kacyakiyor.aracimkacyakti.API.Araclar
-import com.kacyakiyor.aracimkacyakti.API.AraclarCevap
-import com.kacyakiyor.aracimkacyakti.Adapter.AraclarAdapter
-import com.kacyakiyor.aracimkacyakti.databinding.ActivityAracEkleBinding
-import com.kacyakiyor.aracimkacyakti.databinding.ActivityAraclarimBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -34,48 +28,6 @@ class AraclarimActivity : AppCompatActivity() {
         val sp = this.getSharedPreferences("degerler", MODE_PRIVATE)
         val id = sp.getString("id","idyok")
         Log.e("id", id.toString())
-        val drawarLayout = binding.drawarLayout
-        val imgMenu = binding.imgMenu
-        val navView = findViewById<NavigationView>(R.id.navDawar)
-        navView.itemIconTintList = null
-        imgMenu.setOnClickListener {
-            drawarLayout.openDrawer(GravityCompat.START)
-        }
-        navView.setNavigationItemSelectedListener(object :
-            NavigationView.OnNavigationItemSelectedListener {
-            override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
-                when (menuItem.getItemId()) {
-                    R.id.MenuActivity -> {
-                        startActivity(Intent(applicationContext, MenuActivity::class.java))
-                    }
-                    R.id.AracEkleActivity->{
-                        startActivity(Intent(applicationContext,AracEkleActivity::class.java))
-                    }
-                    R.id.HesabimActivity->{
-                        startActivity(Intent(applicationContext,HesabimActivity::class.java))
-                    }
-                    R.id.ContactActivity->{
-                        startActivity(Intent(applicationContext,ContactActivity::class.java))
-                    }
-                    R.id.MainActivity->{
-                        val sp = getSharedPreferences("checkbox", MODE_PRIVATE)
-                        val editor = sp.edit()
-                        editor.putString("remember", "false")
-                        editor.apply()
-                        Toast.makeText(applicationContext, "Başarıyla Çıkış Yapıldı", Toast.LENGTH_SHORT).show()
-                        startActivity(Intent(applicationContext,MainActivity::class.java))
-
-                    }
-                    R.id.FisEkleActivity->{
-                        startActivity(Intent(applicationContext,FisEkleActivity::class.java))
-                    }
-                    R.id.FislerimActivity->{
-                        startActivity(Intent(applicationContext,FislerimActivity::class.java))
-                    }
-                }
-                return true
-            }
-        })
         kdi.araclarim(id.toString()).enqueue(object :Callback<AraclarCevap>{
             override fun onResponse(call: Call<AraclarCevap>, response: Response<AraclarCevap>) {
                 val arac_liste = response.body()?.dersler
